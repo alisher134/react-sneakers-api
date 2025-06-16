9abc141249d0943ec33dfa86685682c413e4ddd1
Alisher Rakhmanov <alisherrakhmanov134@gmail.com>
2025-06-16 11:38:36 +0500

    chore: add config for env variables

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
 
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  const configService = app.get(ConfigService);
+
+  const appPrefix = configService.get<string>('APP_PREFIX', 'api/v1');
+  app.setGlobalPrefix(appPrefix);
+
+  const port = configService.get<number>('PORT', 8080);
+  await app.listen(port, () => {
+    logger.log(`🚀 Server is running at port: ${port}`);
+  });
 }
 bootstrap().catch((err) => console.log(err));
